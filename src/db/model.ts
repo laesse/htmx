8666eee48d6bcbe7eea76eea299e480b7b993790
Laesse <lars@volkheimer.dev>
2023-07-09 23:13:17 +0200

    add likeing

--- src/db/model.ts
+++ src/db/model.ts
@@ -1,7 +1,7 @@
 import { getDbConnection } from "./db-con";
 import type { NewUser, NewPost } from "./schema";
 import { user, post, like } from "./schema";
-import { eq, desc } from "drizzle-orm";
+import { eq, desc, sql, and } from "drizzle-orm";
 
 export const insertNewUser = async (newUser: NewUser) => {
   const db = getDbConnection();
@@ -26,4 +26,43 @@ export type PostWithAuthor = Awaited<ReturnType<typeof findPostPage>>[0];
 export const insertPost = async (newPost: NewPost) => {
   const db = getDbConnection();
   return db.insert(post).values(newPost);
+};
+
+export const deletePost = async (id: string) => {
+  const db = getDbConnection();
+  return db.transaction(async (tx) => {
+    await tx.delete(like).where(eq(like.postId, id));
+    await tx.delete(post).where(eq(post.id, id));
+  });
+};
+
+export const getLikeCount = async (postId: string) => {
+  const db = getDbConnection();
+  return db
+    .select({ cnt: sql<number>`count(1)` })
+    .from(like)
+    .where(eq(like.postId, postId));
+};
+export const hasUserLikedPost = async (userId: string, postId: string) => {
+  const db = getDbConnection();
+  return (
+    (
+      await db
+        .select()
+        .from(like)
+        .where(and(eq(like.userId, userId), eq(like.postId, postId)))
+    ).length > 0
+  );
+};
+
+export const likePost = async (userId: string, postId: string) => {
+  const db = getDbConnection();
+  return db.insert(like).values({ userId, postId });
+};
+
+export const remvoeLike = async (userId: string, postId: string) => {
+  const db = getDbConnection();
+  return db
+    .delete(like)
+    .where(and(eq(like.userId, userId), eq(like.postId, postId)));
 };
